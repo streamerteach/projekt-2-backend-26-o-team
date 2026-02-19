1ad3d74fbcb78b0a51a97fbc409c7b53eb10f9bf
Erik Leppänen <leppanen.erik0@gmail.com>
2026-02-19 13:53:28 +0200

    register

--- root/scripts/registerHandler.php
+++ root/scripts/registerHandler.php
@@ -2,28 +2,40 @@
 include "./databaseConnection.php";
 include "./sessionhandler.php";
 
+ini_set('display_errors', '1');
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
+$username = "alfred";
+$realname = "alfred krupp";
+$zipcode = 04250;
+$bio = "bowow";
+$salary = 20000;
+$preference = 1;
+$email = "alfred@gmail.com";
+$role = 1;
+$password = "password";
 
-function create_user($username, $realname, $zipcode, $bio, $salary, $preference, $email, $likes, $role, $password) {
-
-    
-    $passhash = password_hash($password,null);
-    
-    $sql = "INSERT INTO `profiles` (`id`, `username`, `realname`, `zipcode`, `bio`, `salary`, `preference`, `email`, `likes`, `role`, `passhash`) VALUES 
-    (NULL, :username, :realname, :zipcode, :bio, :salary, :preference, :email, NULL, 1, :passhash);";
+$passhash = password_hash($password, PASSWORD_DEFAULT);
+try{
+    $sql = "INSERT INTO profiles 
+(username, realname, zipcode, bio, salary, preference, email, likes, role, passhash)
+VALUES 
+(:username, :realname, :zipcode, :bio, :salary, :preference, :email, NULL, 1, :passhash)";
 
 $stmt = $conn->prepare($sql);
 $stmt->execute([
- ':username' => $username,
- 'realname' => $realname,
- ':zipcode' => $zipcode,
- ':bio' => $bio,
- ':salary' => $salary,
- ':preference' => $preference,
- ':email' => $email,
- ':passhash' => $passhash
+    ':username' => $username,
+    ':realname' => $realname,
+    ':zipcode' => $zipcode,
+    ':bio' => $bio,
+    ':salary' => $salary,
+    ':preference' => $preference,
+    ':email' => $email,
+    ':passhash' => $passhash
 ]);
-
+echo ("user created");
+} catch (e) {
+    echo("failed");
 }
 
 ?>
